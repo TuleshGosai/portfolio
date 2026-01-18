032be554cc3301c55b17b943d7fbebb4c3cf1cb0
Tulesh gosai <tuleshzeronsec@gmail.com>
2026-01-18 16:45:33 +0530

    splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import Sidebar from './layout/Sidebar'
-import Navigation from './layout/Navigation'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import ToastContainer from './components/ToastContainer'
+import { useState } from "react"
+import Sidebar from "./layout/Sidebar"
+import Navigation from "./layout/Navigation"
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import ToastContainer from "./components/ToastContainer"
+import SplashScreen from "./components/SplashScreen"
 
-function App() {
+const App: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true)
+
+  if (loading) {
+    return <SplashScreen onFinish={() => setLoading(false)} />
+  }
+
   return (
     <div className="min-h-screen bg-[#F0F0F6]">
       <Sidebar />
       <Navigation />
       <ToastContainer />
-      
+
       <main className="lg:ml-80 xl:mr-28">
         <Home />
         <Contact />
